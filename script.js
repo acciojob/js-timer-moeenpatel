c2ea6632d6e77266567b74f1e94c3b8d8e82a3e1
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-21 22:20:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 // write js code here if required
+let d=new Date();
+document.getElementById("timer").innerText=`${d.getDate()} / ${d.getMonth()} / ${d.getFullYear()}, ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
