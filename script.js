b6cdd87a36044f7b64c05af5f0e094e0ed7abbdc
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-22 11:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-function clock(){
+let para = document.getElementById("timer");
 
-	let today = new Date();
-	let month = today.getMonth()+1;
-	let day = today.getDate();
-	let year = today.getFullYear();
-	let hours = today.getHours();
-	
-	let minutes = today.getMinutes();
-	if(minutes<10)
-	{
-		minutes = "0"+minutes;
-	}
-
-	let seconds = today.getSeconds();
-	if(seconds<10){
-		seconds = "0"+seconds;
-	}
-	
-	let time = document.getElementById("timer");
-	time.innerHTML = day+"/"+ month+"/"+ year +"," + " " + hours 
-		+":"+ minutes + ":" + seconds;
-
-}
+setInterval(()=>{
+	para.innerText = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()},  ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+},1000)
