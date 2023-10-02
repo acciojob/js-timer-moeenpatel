f7cc680988dcbac35c8fdeab121ce95c385c4b8d
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-10-02 20:45:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function updateTimer() {
-    const timerElement = document.getElementById('timer');
-    
-    // Get the current date and time
-    const now = new Date();
-    
-    // Extract date and time components
-    const day = String(now.getDate()).padStart(2, '0');
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-based
-    const year = now.getFullYear();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    
-    // Display the timer in the specified format
-    const timerString = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
-    timerElement.textContent = timerString;
+function clock(){
+
+	let today = new Date();
+	let month = today.getMonth()+1;
+	let day = today.getDate();
+	let year = today.getFullYear();
+	let hours = today.getHours();
+	
+	let minutes = today.getMinutes();
+	if(minutes<10)
+	{
+		minutes = "0"+minutes;
+	}
+
+	let seconds = today.getSeconds();
+	if(seconds<10){
+		seconds = "0"+seconds;
+	}
+	
+	let time = document.getElementById("timer");
+	time.innerHTML = day+"/"+ month+"/"+ year +"," + " " + hours 
+		+":"+ minutes + ":" + seconds;
+
 }
-
-// Update the timer every second
-setInterval(updateTimer, 1000);
-
-// Call the updateTimer function initially to avoid a delay in displaying the timer
-updateTimer();
