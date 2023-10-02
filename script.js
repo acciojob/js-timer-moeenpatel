9279ce3e63f14a260e9bffb786557a6d0eb5c2dd
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-10-02 20:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-let para = document.getElementById("timer");
+function updateTimer() {
+    const timerElement = document.getElementById('timer');
+    
+    // Get the current date and time
+    const now = new Date();
+    
+    // Extract date and time components
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    // Display the timer in the specified format
+    const timerString = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+    timerElement.textContent = timerString;
+}
 
-setInterval(()=>{
-	para.innerText = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()},  ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
-},1000)
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+// Call the updateTimer function initially to avoid a delay in displaying the timer
+updateTimer();
